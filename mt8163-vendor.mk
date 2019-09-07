a021ef3c778d8f6a7af4658d23b4a746c8991023
Kai Jones <zaksenoj@icloud.com>
2019-09-07 09:35:09 -0700

    add required changes for Q

--- mt8163-vendor.mk
+++ mt8163-vendor.mk
@@ -14,7 +14,14 @@
 # limitations under the License.
 #
 
+LOCAL_PATH_VENDOR := vendor/amazon/mt8163
 
 # Thermal Manager
 PRODUCT_PARCKAGES += \
      thermal_manager
+
+
+# Debug Tool
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH_VENDOR)/proprietary/debug/debug_tool:$(TARGET_COPY_OUT_VENDOR)/bin/debug_tool
+
