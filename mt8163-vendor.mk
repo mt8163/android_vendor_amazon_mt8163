0c07bf128555771c87c881c24f641352ca203c46
Kai Jones <kvjones0243@proton.me>
2022-12-18 14:11:41 -0800

    mt8163:Remove unused packages
    Change-Id: If8fc93aaa41fc038179ad16262ead28cff70290e
    Signed-off-by: Kai Jones <kvjones0243@proton.me>

--- mt8163-vendor.mk
+++ mt8163-vendor.mk
@@ -20,26 +20,18 @@ LOCAL_PATH_VENDOR := vendor/amazon/mt8163
 PRODUCT_PACKAGES += \
    thermal_manager
 
-# Email
-PRODUCT_PACKAGES += \
-   Email
-
 # Debugging tool
 PRODUCT_PACKAGES +=\
    debug_tool
-   
-# vold fix 
+
+# vold fix
 PRODUCT_PACKAGES += \
    vold_fix
-   
-# MediatekParts
-PRODUCT_PACKAGES += \
-   MediatekParts
 
 # Kernel modules loader
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH_VENDOR)/proprietary/external/insmod/init.insmod.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.insmod.sh \
-   $(LOCAL_PATH_VENDOR)/proprietary/external/insmod/init.insmod.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/init.insmod.cfg 
+   $(LOCAL_PATH_VENDOR)/proprietary/external/insmod/init.insmod.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/init.insmod.cfg
 
 PRODUCT_PROPERTY_OVERRIDES  += \
    ro.config.hw_quickpoweron=true \
